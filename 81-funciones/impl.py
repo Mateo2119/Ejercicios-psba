3f76804540942dab270cc7b1a7c2a6323dffb4b2
Mateo2119 <31406793+Mateo2119@users.noreply.github.com>
2020-02-24 20:04:15 -0500

    Add files via upload

---
+++ 81-funciones/impl.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+@author: lenovo
+"""
+import modulo as m
+
+a = int(input("Digite el primer numero: "))
+b = int(input("Digite el segundo numero: "))
+
+print(f"Suma: {m.suma(a,b)}")
+print(f"Resta: {m.resta(a,b)}")
+print(f"Multiplicacion: {m.multi(a,b)}")
+print(f"Division: {m.div(a,b)}")
+print(f"Potencia: {m.pot(a,b)}")
---
+++ 81-funciones/modulo.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+81. Crear un módulo de funciones aritméticas que realicen las operaciones de suma, resta,
+multiplicación, división y potencia de enteros, escribir un programa que importe este
+módulo y use estas funciones para operar con números capturados desde el teclado.
+"""
+
+def suma(a,b):
+    return a + b
+
+def resta(a,b):
+    return a - b
+
+def multi(a,b):
+    return a*b
+
+def div(a,b):
+    return a/b
+
+def pot(a,b):
+    return a**b
+        
